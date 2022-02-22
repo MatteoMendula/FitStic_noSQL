a1ae6cd0fe2ff5400f0af7e9670883cf030f9bd7
Matteo Mendula <33020977+MatteoMendula@users.noreply.github.com>
2022-02-22 13:39:25 +0100

    node js code connection - cassandra drivers

---
+++ 1. Cassandra/Cassandra NodeJs/connect-database.js
@@ -0,0 +1,26 @@
+const { Client } = require("cassandra-driver");
+
+async function run() {
+    const client = new Client({
+      cloud: {
+        secureConnectBundle: "./secure-connect-testcassandra.zip",
+      },
+      credentials: {
+        username: "OHhXyNaniDEJbNpOjUOytoXg",
+        password: "sq.2vB6Rg60tSZDuy0X5IcslKR30-bWrPdn1dWCbZbyrRfplfFylvv5bOr_r1gN8r95Hy1wh4fAdObu+_Soqs0wGPWb.Ej9LMuGoZZ4xwYwll2Rt_KRP6G7ou5nJ-t_j",
+      },
+    });
+  
+    await client.connect();
+  
+    // Execute a query
+    // const rs = await client.execute("SELECT * FROM system.local");
+    let rs = await client.execute('USE "TestCassandraKeySpace"');
+    rs = await client.execute('SELECT * FROM users;');
+    console.log(`Your cluster returned ${JSON.stringify(rs)} row(s)`);
+  
+    await client.shutdown();
+}
+  
+// Run the async function
+run();
